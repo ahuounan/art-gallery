13d8c5b700cfa53ef7a70658b084b3829381f579
Alan Hu <on.alanhu@gmail.com>
2020-03-01 11:53:20 +0000

    fix: linting and formatting tooling

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,52 @@
 const path = require('path');
 
 module.exports = {
-    mode: "development",
-    devtool: "source-map",
-    devServer: {
-        hot: true
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        modules: ["src", "node_modules"]
-    },
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    hot: true
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    modules: ['src', 'node_modules']
+  },
 
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                options: { cacheDirectory: true },
-            },
-            {
-                test: /\.ts(x?)$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "ts-loader",
-                        options: {
-                            transpileOnly: true,
-                        }
-                    }
-                ]
-            },
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        options: { cacheDirectory: true }
+      },
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true
             }
+          },
+          {
+            loader: 'eslint-loader'
+          }
         ]
-    },
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
-    entry: path.join(__dirname, 'src', '/index.tsx'),
-    output: {
-        filename: 'main.js',
-        publicPath: '/dist',
-        path: path.join(__dirname, 'dist')
-    }
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  entry: path.join(__dirname, 'src', '/index.tsx'),
+  output: {
+    filename: 'main.js',
+    publicPath: '/dist',
+    path: path.join(__dirname, 'dist')
+  }
 };
