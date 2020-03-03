4bd3fb4b3bb6414ac602989f6f82c88744191f61
Alan Hu <on.alanhu@gmail.com>
2020-03-03 10:38:08 +0000

    fix: ci

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   devtool: 'source-map',
   devServer: {
     hot: true
